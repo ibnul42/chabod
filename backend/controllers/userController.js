581e115f2fb4b2774dc5411a832bde7525b41ac2
Ibnul Ashir <ibnulashir42@gmail.com>
2023-01-07 16:57:46 +0600

    user register and login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,79 @@
-const registerUser = (req, res) => {
-    res.json({ message: 'User created'})
-}
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
 
-const LoginUser = (req, res) => {
-    res.json({ message: 'User login'})
-}
+const registerUser = asyncHandler( async(req, res) => {
+    const { name, email, password } = req.body
 
-const getMe = (req, res) => {
+    // check if all fields are inputed
+    if(!name || !email || !password) {
+        res.status(404)
+        throw new Error('Please enter all fields')
+    }
+
+    const userExists = await User.findOne({email})
+
+    // check if the user exists
+    if(userExists) {
+        res.status(404)
+        throw new Error('User already Exist')
+    }
+
+    // hash the password
+    const solt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, solt)
+
+    // create a new user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        console.log(user)
+        res.status(201)
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(404)
+        throw new Error('Invalid user data')
+    }
+})
+
+const LoginUser = asyncHandler( async(req, res) => {
+    const { email, password } = req.body
+
+    if(!email || !password) {
+        res.status(404)
+        throw new Error('Invalid credentials')
+    }
+
+    const user = await User.findOne({ email })
+    if(!user) {
+        res.status(404)
+        throw new Error('Invalid credentials')
+    }
+
+    if(email && (await bcrypt.compare(password, user.password))) {
+        res.status(200)
+        res.json({
+            name: user.name,
+            email: user.email
+        })
+    }  else {
+        res.status(404)
+        throw new Error('Invalid credentials')
+    }
+})
+
+const getMe = asyncHandler( async(req, res) => {
     res.json({ message: 'User display'})
-}
+})
 
 module.exports = {
     registerUser,
